e448caa99b186f70f0af9b1c23d6b9b3bc3ca402
skattex <andreysych@gmail.com>
2017-10-23 23:51:24 +0300

    migration changed
    #48

--- laravel/database/migrations/2017_10_22_000002_update_reply_mode.php
+++ laravel/database/migrations/2017_10_22_000002_update_reply_mode.php
@@ -19,16 +19,6 @@ class UpdateReplyMode extends Migration
 UPDATE `reply_mode` SET `mode`='Numeric/text response' WHERE `id`='1';
 SQL
             );
-        DB::unprepared(<<<SQL
-DELETE FROM `reply_mode` WHERE `id`>3;
-SQL
-    ); DB::unprepared(<<<SQL
-INSERT INTO `reply_mode` VALUES
-(10,'mcq','Multiple Choice',NULL,NULL),
-(11,'order','Correct Order',NULL,NULL),
-(12,'mcqms','Multiple Choice/Multiple Answers',NULL,NULL);
-SQL
-    );
     }
 
     /**
@@ -41,20 +31,6 @@ SQL
         //add fields back
         DB::unprepared(<<<SQL
 UPDATE `reply_mode` SET `mode`='General' WHERE `id`='1';
-SQL
-    );
-        DB::unprepared(<<<SQL
-INSERT INTO `reply_mode` VALUES 
-(4,'mcq3','Multiple Choice 3',NULL,NULL),
-(5,'mcq4','Multiple Choice 4',NULL,NULL),
-(6,'mcq5','Multiple Choice 5',NULL,NULL),
-(7,'mcq6','Multiple Choice 6',NULL,NULL),
-(8,'ascending','Ascending Order',NULL,NULL),
-(9,'descending','Descending Order',NULL,NULL);
-SQL
-    );
-    DB::unprepared(<<<SQL
-DELETE FROM `reply_mode` WHERE `id`>9;
 SQL
     );
     }
--- laravel/database/migrations/2017_10_23_000002_update_question_table.php
+++ laravel/database/migrations/2017_10_23_000002_update_question_table.php
@@ -18,6 +18,25 @@ class UpdateQuestionTable extends Migration
         Schema::table('question', function (Blueprint $table) {
             $table->dropColumn(['option_text', 'option_size', 'size']);
         });
+        DB::unprepared(<<<SQL
+DELETE FROM `reply_mode` WHERE `id`>3;
+SQL
+        );
+        DB::unprepared(<<<SQL
+INSERT INTO `reply_mode` VALUES
+(10,'mcq','Multiple Choice',NULL,NULL),
+(11,'order','Correct Order',NULL,NULL),
+(12,'mcqms','Multiple Choice/Multiple Answers',NULL,NULL);
+SQL
+    );
+        DB::unprepared(<<<SQL
+UPDATE question set reply_mode = 10 WHERE reply_mode in (4,5,6,7);
+SQL
+    );
+        DB::unprepared(<<<SQL
+UPDATE question set reply_mode = 11 WHERE reply_mode in (8,9);
+SQL
+        );
     }
 
     /**
@@ -32,5 +51,27 @@ class UpdateQuestionTable extends Migration
             $table->string('option_size')->nullable();
             $table->integer('size')->default(1);
         });
+        DB::unprepared(<<<SQL
+INSERT INTO `reply_mode` VALUES 
+(4,'mcq3','Multiple Choice 3',NULL,NULL),
+(5,'mcq4','Multiple Choice 4',NULL,NULL),
+(6,'mcq5','Multiple Choice 5',NULL,NULL),
+(7,'mcq6','Multiple Choice 6',NULL,NULL),
+(8,'ascending','Ascending Order',NULL,NULL),
+(9,'descending','Descending Order',NULL,NULL);
+SQL
+    );
+        DB::unprepared(<<<SQL
+DELETE FROM `reply_mode` WHERE `id`>9;
+SQL
+        );
+        DB::unprepared(<<<SQL
+UPDATE question set reply_mode = 7 WHERE reply_mode = 10;
+SQL
+        );
+        DB::unprepared(<<<SQL
+UPDATE question set reply_mode = 8 WHERE reply_mode = 11;
+SQL
+        );
     }
 }
