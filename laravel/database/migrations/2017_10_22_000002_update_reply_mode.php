3a3feb034134a28e30339e1d22e900fba4d9775a
Vo-Vik <vo.vik.first@gmail.com>
2017-10-22 21:26:15 -0400

    Reply mode "general"; change to "numeric/text response";

---
+++ laravel/database/migrations/2017_10_22_000002_update_reply_mode.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateReplyMode extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Table structure for table `answer`
+        DB::unprepared(<<<SQL
+UPDATE `reply_mode` SET `mode`='Numeric/text response' WHERE `id`='1';
+SQL
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //add fields back
+        DB::unprepared(<<<SQL
+UPDATE `reply_mode` SET `mode`='General' WHERE `id`='1';
+SQL
+            );
+    }
+}
