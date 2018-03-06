a673394e68d144d26604366a6af9a3e6f362abd0
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2018-03-06 22:02:11 +0200

    CI: change home template to use Angular Material Cards instead of custom cards

--- ci/src/app/_components/home/home.component.ts
+++ ci/src/app/_components/home/home.component.ts
@@ -5,7 +5,8 @@ import { TopicService } from '../../_services/index';
 @Component({
     moduleId: module.id,
     templateUrl: 'home.component.html',
-    providers: [TopicService]
+    providers: [TopicService],
+    styleUrls: ['home.component.css']
 })
 
 export class HomeComponent implements OnInit {
