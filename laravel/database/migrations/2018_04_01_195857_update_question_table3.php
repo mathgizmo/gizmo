fd2365edb45c321060ca0bbf50c7facfab137ca8
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2018-04-04 15:08:23 +0300

    Admin: merge feedback and explanation fields #160

---
+++ laravel/database/migrations/2018_04_01_195857_update_question_table3.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateQuestionTable3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("UPDATE question SET explanation = CONCAT(explanation, ' ', feedback)");
+        Schema::table('question', function($table) {
+            $table->dropColumn('feedback');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('question', function($table) {
+            $table->string('feedback', 255);
+        });
+    }
+
+}
