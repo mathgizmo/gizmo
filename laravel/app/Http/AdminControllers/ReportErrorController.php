18d072a848b5e9b1dda9c3b34f9ba1f2a58af069
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2018-07-12 17:59:56 +0300

    Admin: Error Reports sorting #206

--- laravel/app/Http/AdminControllers/ReportErrorController.php
+++ laravel/app/Http/AdminControllers/ReportErrorController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use App\ReportError;
 
+use Illuminate\Http\Request;
+
 class ReportErrorController extends Controller
 {
 
-    public function index($type)
+    public function index(Request $request, $type)
     {
-        $error_reports = ReportError::where('declined', $type == 'new' ? 0 : 1)->latest()->get();
-
+        if ($request->has('sort') and $request->has('order')) {
+            $error_reports = ReportError::where('declined', $type == 'new' ? 0 : 1)->orderBy($request->sort, $request->order)->latest()->get();
+        } else {
+            $error_reports = ReportError::where('declined', $type == 'new' ? 0 : 1)->latest()->get();
+        }
         return view('error_report_views.index', compact('error_reports', 'type'));
     }
 
