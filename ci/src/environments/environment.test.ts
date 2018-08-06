b4b6baebb1f1466c4e74819790c5546fbb375a4e
Vo-Vik <vo.vik.first@gmail.com>
2018-08-06 02:36:35 -0400

    Building test build locally.

---
+++ ci/src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://hn.domiv.ca/api', //global API URL (for production build)
+  baseUrl: window.location.origin + '/gizmo' // Base URL here
+  //... more of your variables
+};
