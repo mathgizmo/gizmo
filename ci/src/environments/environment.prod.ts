f3664ed04227e26c06c614f5d1b29197b2564c75
Vo-Vik <vo.vik.first@gmail.com>
2018-08-11 11:39:09 -0400

    Do not include env files in build

--- ci/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://healthnumeracyproject.com/api', //global API URL (for production build)
-  adminUrl: 'http://healthnumeracyproject.com/admin',
-  baseUrl: window.location.origin + '/gizmo' // Base URL here
-  //... more of your variables
-};
