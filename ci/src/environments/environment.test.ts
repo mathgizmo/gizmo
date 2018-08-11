00c3bcc0c9673082963ebf34f3e966ac3b7e900e
Vo-Vik <vo.vik.first@gmail.com>
2018-08-11 11:39:41 -0400

    Do not include env files in git

--- ci/src/environments/environment.test.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://hn.domiv.ca/api', //global API URL (for production build)
-  adminUrl: 'http://hn.domiv.ca/admin',
-  baseUrl: window.location.origin + '/gizmo' // Base URL here
-  //... more of your variables
-};
