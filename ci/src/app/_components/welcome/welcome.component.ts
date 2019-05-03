990e3869255a37a6db04696aada0bfdc860937de
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2019-05-03 16:26:18 +0300

    just refactoring

--- ci/src/app/_components/welcome/welcome.component.ts
+++ ci/src/app/_components/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
-import { AuthenticationService, WelcomeService } from '../../_services/index';
+import { WelcomeService } from '../../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -11,24 +11,17 @@ import { AuthenticationService, WelcomeService } from '../../_services/index';
 })
 
 export class WelcomeComponent implements OnInit {
-    model: any = {};
-    loading = false;
-    error = '';
-
-    title = 'Welcome to Health Numeracy Learning Object!';
-    subtitle = 'Improve your numeracy skills. Sharpen your reasoning. Learn something new.';
-    introduction = 'Once you log in, you will see all topics, organized into units and levels. Each topic contains a sequence of lessons. To complete a lesson, you need to correctly answer several questions (this number can be changed in your settings). There are different ways to advance through the material. You can take a placement test and skip some topics. Or, once inside a topic, you can test out of it and move to the next topic. You can always go back to any topic or any lesson you have completed, to review it.';
+    title: string;
+    subtitle: string;
+    introduction: string;
 
     constructor(
         private router: Router,
         private activatedRoute: ActivatedRoute,
-        private authenticationService: AuthenticationService,
         private welcomeService: WelcomeService) {
     }
 
     ngOnInit() {
-        // reset login status
-        this.authenticationService.logout();
         // get texts
         this.welcomeService.getWelcomeTexts()
             .subscribe(result => {
@@ -46,19 +39,6 @@ export class WelcomeComponent implements OnInit {
                         default:
                             break;
                     }
-                }
-            });
-    }
-
-    login() {
-        this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(result => {
-                if (result === true) {
-                    this.router.navigate(['/']);
-                } else {
-                    this.error = 'Username or password is incorrect';
-                    this.loading = false;
                 }
             });
     }
