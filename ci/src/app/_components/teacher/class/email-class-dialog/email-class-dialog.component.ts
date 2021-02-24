761beca344cbc6dcb99bcc1fb644beb3413e93a1
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2021-02-24 19:46:04 +0200

    CI: Add the capacity for teacher to send email to selected students

--- ci/src/app/_components/teacher/class/email-class-dialog/email-class-dialog.component.ts
+++ ci/src/app/_components/teacher/class/email-class-dialog/email-class-dialog.component.ts
@@ -3,11 +3,13 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 import {BaseDialogComponent} from '../../../dialogs/base-dialog.component';
+import {ClassesManagementService} from '../../../../_services';
 
 @Component({
     selector: 'app-email-class-dialog',
     templateUrl: 'email-class-dialog.component.html',
     styleUrls: ['email-class-dialog.component.scss'],
+    providers: [ClassesManagementService]
 })
 export class EmailClassDialogComponent extends BaseDialogComponent<EmailClassDialogComponent> {
 
@@ -15,6 +17,7 @@ export class EmailClassDialogComponent extends BaseDialogComponent<EmailClassDia
         id: 0,
         name: '',
     };
+    public students = [];
     public mail = {
         class_id: 0,
         subject: '',
@@ -26,6 +29,7 @@ export class EmailClassDialogComponent extends BaseDialogComponent<EmailClassDia
     public editor = ClassicEditor;
 
     constructor(
+        private classService: ClassesManagementService,
         public dialogRef: MatDialogRef<EmailClassDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any) {
         super(dialogRef, data);
@@ -33,6 +37,10 @@ export class EmailClassDialogComponent extends BaseDialogComponent<EmailClassDia
             // tslint:disable-next-line:indent
         	this.class = data.class;
         }
+        this.classService.getStudents(this.class.id)
+            .subscribe(students => {
+                this.students = students;
+            });
     }
 
     resizeDialog() {
