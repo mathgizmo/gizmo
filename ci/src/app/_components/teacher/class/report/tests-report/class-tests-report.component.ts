3d81613a6d78f3018faf5eac8e29c632c7878647
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2021-03-16 21:24:57 +0200

    FIX sort by date #301

--- ci/src/app/_components/teacher/class/report/tests-report/class-tests-report.component.ts
+++ ci/src/app/_components/teacher/class/report/tests-report/class-tests-report.component.ts
@@ -87,8 +87,8 @@ export class ClassTestsReportComponent implements OnInit {
         }
         const compare = (a: number | string, b: number | string, isAsc: boolean, isDate = false) => {
             if (isDate) {
-                const aDate = moment(a, 'YYYY-MM-DD hh:mm A');
-                const bDate = moment(b, 'YYYY-MM-DD hh:mm A');
+                const aDate = a ? moment(a, 'YYYY-MM-DD hh:mm A') : moment(0);
+                const bDate = b ? moment(b, 'YYYY-MM-DD hh:mm A') : moment(0);
                 return (aDate.isBefore(bDate) ? -1 : 1) * (isAsc ? 1 : -1);
             } else {
                 if (typeof a === 'string' || typeof b === 'string') {
