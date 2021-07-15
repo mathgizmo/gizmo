adc01928941de64ec015d86e9f207d37a2919225
Andriy Kuzjmych <andriy.kuzjmych@gmail.com>
2021-07-15 19:24:03 +0300

    change from address to sender email #320

--- laravel/app/Mail/ClassMail.php
+++ laravel/app/Mail/ClassMail.php
@@ -12,17 +12,20 @@ class ClassMail extends Mailable
 
     public $message;
     public $subject;
+    public $user;
 
     public function __construct($subject, $body, $user, $class)
     {
         $this->subject = $subject;
         $this->message = 'Hey there, <br/>you have a new message from <a href="mailto:'.$user->email.'">'.$user->first_name.' '.$user->last_name.'</a> (class '.$class->name.'):<br/>'
             .$body;
+        $this->user = $user;
     }
 
     public function build()
     {
-        return $this->subject($this->subject)
+        return $this->from($this->user->email)
+            ->subject($this->subject)
             ->markdown('mails.default_template');
     }
 }
