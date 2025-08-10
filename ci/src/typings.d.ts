3c06056829014c15c4cb0511e4bf511a3a2cd201
Vo Vik <vovik@Vos-MacBook-Air.local>
2025-08-10 18:43:36 -0400

    angular version update to v13

--- ci/src/typings.d.ts
+++ ci/src/typings.d.ts
@@ -3,3 +3,8 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+/* IE-specific Navigator extensions */
+interface Navigator {
+  msSaveOrOpenBlob?: (blob: Blob, filename?: string) => void;
+}
