f8e0367bad259650b17f354fab40fb33017431da
Vo-Vik <vo.vik.first@gmail.com>
2025-08-20 14:41:21 +0000

    prod backup changes

--- backup/do_restore.sh
+++ backup/do_restore.sh
@@ -8,7 +8,8 @@ DB_PORT=3306
 . ../laravel/.env
 . .dropbox_uploader
 #use Los Angeles time zone to make sure that we have back up
-date=$(TZ=":America/Los_Angeles" date +"%d-%b-%Y")
+#date=$(TZ=":America/Los_Angeles" date +"%d-%b-%Y")
+date="01-Aug-2025"
 
 credentialsFile=./.mysql-credentials.cnf
 echo "[client]" > $credentialsFile
@@ -19,25 +20,25 @@ echo "host=$DB_HOST" >> $credentialsFile
 umask 177
 
 #upload to dropbox
-./dropbox_uploader.sh -f .dropbox_uploader download /$DROP_BOX_FOLDER/$DB_DATABASE-$date.sql.gz ./$DB_DATABASE-$date.sql.gz
-if [ ! -d "../laravel/storage/app" ]; then
-  mkdir ../laravel/storage/app
-  chmod 755 ../laravel/storage/app
-fi
-if [ ! -d "../laravel/storage/app/public" ]; then
-  mkdir ../laravel/storage/app/public
-  chmod 755 ../laravel/storage/app/public
-fi
-if [ ! -d "../laravel/storage/app/public/uploads" ]; then
-  mkdir ../laravel/storage/app/public/uploads
-  chmod 755 ../laravel/storage/app/public/uploads
-fi
-UPLOADS=$(./dropbox_uploader.sh -f .dropbox_uploader list /$DROP_BOX_FOLDER/uploads/ | awk '{ print $3}' | grep -v '\.\.\.' )
-for u in $UPLOADS
-do
-    echo "Copping $u ..."
-    ./dropbox_uploader.sh -f .dropbox_uploader download /$DROP_BOX_FOLDER/uploads/$u ../laravel/storage/app/public/uploads/$u
-done
+./dropbox_uploader.sh -f .dropbox_uploader download /$DROP_BOX_FOLDER/healthnu_gizmo-$date.sql.gz ./$DB_DATABASE-$date.sql.gz
+#if [ ! -d "../laravel/storage/app" ]; then
+#  mkdir ../laravel/storage/app
+#  chmod 755 ../laravel/storage/app
+#fi
+#if [ ! -d "../laravel/storage/app/public" ]; then
+#  mkdir ../laravel/storage/app/public
+#  chmod 755 ../laravel/storage/app/public
+#fi
+#if [ ! -d "../laravel/storage/app/public/uploads" ]; then
+#  mkdir ../laravel/storage/app/public/uploads
+#  chmod 755 ../laravel/storage/app/public/uploads
+#fi
+#UPLOADS=$(./dropbox_uploader.sh -f .dropbox_uploader list /$DROP_BOX_FOLDER/uploads/ | awk '{ print $3}' | grep -v '\.\.\.' )
+#for u in $UPLOADS
+#do
+#    echo "Copping $u ..."
+#    ./dropbox_uploader.sh -f .dropbox_uploader download /$DROP_BOX_FOLDER/uploads/$u ../laravel/storage/app/public/uploads/$u
+#done
 
 # Dump database into SQL file
 #mysqldump --defaults-extra-file=$credentialsFile $DB_DATABASE | gzip> $DB_DATABASE-$date.sql.gz
